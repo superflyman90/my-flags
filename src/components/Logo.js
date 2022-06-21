e7f150efcc4305fa874c05c25f8a749090af1196
superflyman90 <superflyman90@gmail.com>
2022-06-21 14:58:41 +0200

    second deploy

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ const Logo = () => {
     return (
         <div className="logo">
             <img src="./logo192.png" alt="logo-react" />
-            <h3>Manfred's flag</h3>
+            <h3>Manfred's flags</h3>
         </div>
     );
 };
--- src/pages/About.js
+++ src/pages/About.js
@@ -7,11 +7,11 @@ const About = () => {
         <div>
             <Logo />
             <Navigation />
-            <h2>About</h2>
+            <h2>A propos</h2>
             <br />
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum saepe laboriosam laudantium magnam. Aliquam possimus odit at cumque laudantium voluptate natus minima id, sunt officiis, in aliquid libero molestiae animi, veritatis architecto. Nemo repellat, delectus doloremque minus dolorem voluptates, natus repellendus modi non possimus iusto veniam voluptas blanditiis quos numquam? Delectus, veniam porro adipisci inventore animi tempora quaerat minus dolorem, placeat, a recusandae praesentium quas facilis! Iste facilis consequatur mollitia dolorum magni laudantium at temporibus odit corporis ipsam sint debitis consectetur nihil ratione cupiditate, perferendis culpa recusandae aperiam repudiandae vitae numquam ea praesentium! Quas quisquam eaque quis necessitatibus perspiciatis accusantium?</p>
+            <p>Ce site a été développé à l'aide de la bibliothèque React JS. Pour la mise en place des fonctionnalités j'ai suivi l'excellente formation de "FromScratch" sur youtube.com.</p>            
             <br />
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit doloribus quasi odit magni. Quo nesciunt fuga unde nisi, eaque ad. Et amet earum natus rem illo ipsam tenetur non aliquam reprehenderit, est quaerat corporis, placeat error vitae accusantium? Officia maiores rerum tenetur, ipsum delectus non unde incidunt eaque id sed soluta ad quo quibusdam corporis consequuntur amet pariatur a ullam nihil quod molestiae! Optio eligendi adipisci soluta at! Sint quae in ipsam sunt, debitis aperiam reprehenderit nihil doloribus maxime pariatur.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit doloribus quasi odit magni. Quo nesciunt fuga unde nisi, eaque ad. Et amet earum natus rem illo ipsam tenetur non aliquam reprehenderit, est quaerat corporis, placeat error vitae accusantium? </p>
         </div>
     );
 };
